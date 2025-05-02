9416949edf91e4d7749fddd5043c5b61c2a93469
PrajwalaNagekar <prajwalanagekar@gmail.com>
2025-05-02 11:30:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express, { json } from 'express'
+import mongoose from "mongoose";
+import AdminRoutes from './src/routes/adminroutes.js'
+const app = express();
+const router = express.Router();
+import dotenv from 'dotenv'
+import cors from 'cors';
+import connectToDb from './src/database/index.js';
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json())
+app.use("/api", AdminRoutes);
+// app.listen(PORT, console.log(`Backend is running on port:${PORT}`));
+
+const startServer = async () => {
+    try {
+        await connectToDb()
+        app.listen(PORT, () => {
+            console.log(`server running on ${PORT}`);
+        })
+    } catch (error) {
+        console.log("MONGO db connection failed !!! ", error);
+    }
+}
+startServer();
---
+++ src/routes/adminroutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+router.post('/admin-login',(req,res)=>{
+    console.log('hi');
+})
+export default router;
